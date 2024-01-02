21a9eeea4800a3156d2e6fe5d4a2134a10e559c1
Iago Morais <morais.sda@gmail.com>
2024-01-02 13:58:17 -0300

    jogo adivinha

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-
-alert('Morais jogos Boas Vindas');
-
-let secretNumber = 5;
-console.log('numero ', secretNumber);
-let guess;
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
 let tentativas = 1;
 
-
-//eenquanto
-while (guess != secretNumber) {// != e usado para se uma açao nao for igual a x 
-    guess = prompt('choose a number between 1 and 30?');
-    //se for
-    if (guess == secretNumber) {
-        alert(`that s right! did you discover the secret number ${secretNumber} com ${tentativas} tentativas`);
-    } else {//se nao
-        if (secretNumber < guess) {
-            alert(`O numeor secreto e menor que ${guess}`);
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O numeor secreto e maior que ${guess}`);
+            alert(`O número secreto é maior que ${chute}`);
         }
-        //comando exenplo > tentativas = tentativas + 1;
-        tentativas++
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
